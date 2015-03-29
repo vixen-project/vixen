0608400a9da92b31370b533edc03bf83945ad789
Prabhu Ramachandran <prabhu@enthought.com>
2015-03-29 13:21:04 +0530

    Fix silly error in process_file.

--- vixen/process_file.py
+++ vixen/process_file.py
@@ -12,17 +12,15 @@ def _process_avi(path):
     video = abspath(path)
     webm_video = splitext(video)[0] + '.webm'
     poster = splitext(video)[0] + '.jpg'
-    size = os.stat(video).st_size
 
     import subprocess
-    if size > 0:
-        if not exists(webm_video):
-            command = ["ffmpeg", "-i", video, webm_video]
-            out = subprocess.check_output(command, stderr=subprocess.STDOUT)
-        if not exists(poster):
-            command = ["ffmpeg", "-i", video,
-                        "-ss", "0", "-vframes", "1", poster]
-            out = subprocess.check_output(command, stderr=subprocess.STDOUT)
+    if not exists(webm_video):
+        command = ["ffmpeg", "-i", video, webm_video]
+        out = subprocess.check_output(command, stderr=subprocess.STDOUT)
+    if not exists(poster):
+        command = ["ffmpeg", "-i", video,
+                    "-ss", "0", "-vframes", "1", poster]
+        out = subprocess.check_output(command, stderr=subprocess.STDOUT)
     return webm_video, poster
 
 
@@ -32,8 +30,9 @@ def process_file(path):
     This converts any AVI files to webm so they can be viewed, parses
     the file path to determine the tags and returns a bunch of tags.
     """
+    size = os.stat(path).st_size
     ext = splitext(path)[1]
-    if ext not in ['.avi', '.AVI']:
+    if (ext not in ['.avi', '.AVI']) or (size == 0):
         return
 
     view, poster = _process_avi(path)
